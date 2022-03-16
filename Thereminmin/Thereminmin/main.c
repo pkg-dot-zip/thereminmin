1a782a127d7c68065a18b4699b811f8e829070ff
Simon <dutchsimon9@gmail.com>
2022-03-16 13:18:24 +0100

    Project init

---
+++ Thereminmin/Thereminmin/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 3/16/2022 1:17:49 PM
+ *  Author: dutch
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
