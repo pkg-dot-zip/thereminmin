954d39c0adede4d640d5d0a4eaabb8aea3332689
Simon <dutchsimon9@gmail.com>
2022-03-29 12:44:45 +0200

    Created methods for calculating notes

---
+++ Thereminmin/Thereminmin/audio_math.c
@@ -0,0 +1,30 @@
+/*
+ * audio_math.c
+ *
+ * Created: 29/03/2022 12:34:37
+ *  Author: ZimonIsHim
+ */ 
+
+static double get_pow(double value);
+int get_note(int note, int octave);
+
+/*
+Frequencies are calculated using the following formula: 2^N/12 * 440.
+Where:
+* N: The amount of halfsteps/semitones compared to the note A4.
+* 12: The amount of halfsteps in an octave (12 halfsteps/semitones).
+* 440: The frequency in Hertz of the note A4.
+*/
+int get_note(int note, int octave) {
+	double noteFrequency;
+	int halfStepDifference = 0; // A is 0, C is -9, A# is 1, B is 2 etc..
+	
+	 //Formulas used to calculate the frequency.
+	 halfStepDifference += (octave - 4) * 12;
+	 noteFrequency = get_pow(halfStepDifference / 12.0) * 440.0;
+	 return noteFrequency;
+}
+
+static double get_pow(double value) {
+	return value * value;
+}
---
+++ Thereminmin/Thereminmin/audio_math.h
@@ -0,0 +1,16 @@
+/*
+ * audio_math.h
+ *
+ * Created: 29/03/2022 12:40:33
+ *  Author: dutch
+ */ 
+
+
+#ifndef AUDIO_MATH_H_
+#define AUDIO_MATH_H_
+
+
+
+
+
+#endif /* AUDIO_MATH_H_ */
