037dfe85e06327cbafc2f207271a1b9b62ee1a2a
Stijn van Tilburg <stijnvtilburg@hotmail.com>
2022-03-29 15:51:27 +0200

    Start buzzer

--- Thereminmin/Thereminmin/main.c
+++ Thereminmin/Thereminmin/main.c
@@ -4,6 +4,7 @@
 #include <util/delay.h>
 #include <string.h>
 #include <stdlib.h>
+#define INTERVAL  		2273
 
 #define BIT(x)			(1 << (x))
 
@@ -18,11 +19,22 @@ void display_distance_on_lcd() {
 	lcd_display_text(string);
 }
 
+// Initialize timer 1: fast PWM at pin PORTB.6 (hundredth ms)
+inline void timer1Init( void )
+{
+	OCR1A = 0;					// RED, default, off
+	OCR1B = 0;					// GREEN, default, off
+	OCR1C = 0;					// BLUE, default, off
+	TCCR1A = 0b10101001;		// compare output OC1A,OC1B,OC1C
+	TCCR1B = 0b00001011;		// fast PWM 8 bit, prescaler=64, RUN
+}
 
 int main(void)
 {
 	US_init();
-		
+		DDRB = 0xFF;					// set PORTB for compare output 
+	DDRA = 0xFF;					// set PORTA for output in main program
+	timer1Init();
 	while(1)
 	{
 
@@ -31,6 +43,8 @@ int main(void)
 		US_trigger();
 		wait(250);
 		display_distance_on_lcd();
+		if (distance > 30) OCR1A = (char)distance;			// toggle bit 7 PORTA
+		
 		
 	}
 }
