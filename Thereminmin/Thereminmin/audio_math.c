060fb06c5db4a60156dececf58178de695f4021c
Simon <dutchsimon9@gmail.com>
2022-03-30 12:47:31 +0200

    Hardcoded return values

--- Thereminmin/Thereminmin/audio_math.c
+++ Thereminmin/Thereminmin/audio_math.c
@@ -56,5 +56,51 @@ char get_musical_value(double distance) {
 		}
 	}
 	
-	return (char)(140 - 10 * notes_to_increment);
+	int value_to_return = 0;
+	
+	switch(notes_to_increment) {
+		case 0:
+		value_to_return = 140;
+		break;
+		case 1:
+		value_to_return = 130;
+		break;
+		case 2:
+		value_to_return = 120;
+		break;
+		case 3:
+		value_to_return = 110;
+		break;
+		case 4:
+		value_to_return = 100;
+		break;
+		case 5:
+		value_to_return = 90;
+		break;
+		case 6:
+		value_to_return = 80;
+		break;
+		case 7:
+		value_to_return = 70;
+		break;
+		case 8:
+		value_to_return = 60;
+		break;
+		case 9:
+		value_to_return = 50;
+		break;
+		case 10:
+		value_to_return = 40;
+		break;
+		case 11:
+		value_to_return = 30;
+		break;
+		case 12:
+		value_to_return = 20;
+		break;
+	}
+	
+	return value_to_return;
+	
+	// return (char)(140 - 10 * notes_to_increment); // Old calculation (inaccurate).
 }
