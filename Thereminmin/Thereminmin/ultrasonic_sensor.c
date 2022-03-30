2a2fad6f781529425b173d6402f61349815f7598
Stijn van Tilburg <stijnvtilburg@hotmail.com>
2022-03-30 14:33:33 +0200

    Refactored ultrasonic sensor

--- Thereminmin/Thereminmin/ultrasonic_sensor.c
+++ Thereminmin/Thereminmin/ultrasonic_sensor.c
@@ -11,12 +11,12 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define  TRIGGER_PIN	0	/* Trigger pin */
-#define  ECHO_PIN	7	/* Echo pin */
+#define  TRIGGER_PIN	0	// Trigger pin 
+#define  ECHO_PIN	7	// Echo pin 
 #define TICK_CM 58.0
 
 char ticksOnTrigger = 0;
-double distance = 0;
+double distance = 0; //extern
 
 void wait( int ms ) {
 	for (int i=0; i<ms; i++) {
@@ -24,43 +24,35 @@ void wait( int ms ) {
 	}
 }
 
-ISR(TIMER2_OVF_vect)
-{
-	TCNT2 = 0;
+//This intterupt triggers after an overflow
+ISR(TIMER2_OVF_vect) {
+	TCNT2 = 0; // reset the timer after an overflow
 }
 
-ISR ( INT7_vect )
-{
-	// Setting the time from the timer on PE7 going high
+ISR ( INT7_vect ) {
+	// 
 	if(PINE & (1 << ECHO_PIN)) {
-		ticksOnTrigger = TCNT2;
-	}
-	else
-	{
+		ticksOnTrigger = TCNT2; //get the amount of ticks at the start of the echo
+	} else {
 		unsigned char diff = TCNT2 - ticksOnTrigger;        // Getting the difference from timer and start
-
-		distance = ( diff * ( 32.0 / TICK_CM ) );    // Calculating cm
-
-
-		ticksOnTrigger = 0;                                    // resetting ticksOnTrigger
+		distance = ( diff * ( 32.0 / TICK_CM ) );    // distance calculation
+		ticksOnTrigger = 0; // resetting the ticks since calculation echo was detected
 	}
 }
 
 void US_init_pins() {
-	DDRD = 0xFF;
-	
-	EICRB |= 0x40;			//
+	EICRB |= 0x40;			// any logical change on INT7 generates an interrupt request
 	EIMSK |= 0x80;			// Enable INT7
 	
-	DDRE |= (1 << TRIGGER_PIN);
-	DDRE &= ~(1 << ECHO_PIN);
+	DDRE |= (1 << TRIGGER_PIN); // PORTE TRIGGER_PIN to output
+	DDRE &= ~(1 << ECHO_PIN); // PORTE ECHO_PIN to input
 }
 
-void Timer_init() {
-	TCNT2 = 0;	/* Clear Timer counter */
-	TIMSK |= 0b01000000; /* Enables overflow interrupt for timer 2, TOV2 bit is set in the Timer/Counter Interrupt Flag Register – TIFR. */
-	TCCR2 = 0b00001100;
-	sei();			/* Enable global interrupt */
+void timer_init() {
+	TCNT2 = 0;	// Clear Timer counter 
+	TIMSK |= 0b01000000; // Enables overflow interrupt for timer 2.
+	TCCR2 = 0b00001100; // Timer mode of operation: pwm phase correct, prescaler: 256
+	sei();			// Enable global interrupt 
 }
 
 
@@ -71,7 +63,8 @@ void US_trigger() {
 	PORTE &= ~(1 << TRIGGER_PIN);
 }
 
+//initialize the ultrasonic sensor
 void US_init() {
-	Timer_init();
+	timer_init();
 	US_init_pins();
 }
