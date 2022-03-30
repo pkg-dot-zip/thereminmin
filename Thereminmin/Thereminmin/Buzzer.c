3ab877dd4e86c63060dd274ee2ddcb4ab74594f5
Stijn van Tilburg <stijnvtilburg@hotmail.com>
2022-03-30 14:57:18 +0200

    More documentation in Buzzer.c

--- Thereminmin/Thereminmin/Buzzer.c
+++ Thereminmin/Thereminmin/Buzzer.c
@@ -12,11 +12,11 @@
 void BUZZER_init( void )
 {
 	DDRB = 0xFF;					// set PORTB for compare output
-	OCR1A = 140;					//
-	TCCR1A = 0b01101011;		// compare output OC1A,OC1B,OC1C
-	TCCR1B = 0b00011011;		// fast PWM 8 bit, prescaler=64, RUN
+	OCR1A = 140;					// sets the top value of Timer 1 to 140
+	TCCR1A = 0b01101011;		// sets timer 1 to: compare output OC1A,OC1B,OC1C
+	TCCR1B = 0b00011011;		// sets timer 1 to:fast PWM 8 bit, prescaler=64, RUN
 }
 
 void BUZZER_change_frequency(char frequency) {
-	OCR1A = frequency;
+	OCR1A = frequency; //changes the top value of the timer which causes a shift in pwm value
 }
